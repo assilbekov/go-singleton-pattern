b5d84c36bb44f203e60f5e898df84a57374eaacb
assilbekov <askhat.assilbekov@gmail.com>
2024-05-08 17:21:47 -0300

    implement Singleton

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"singleton/pkg"
+)
+
+var singleton *pkg.Singleton
 
 func init() {
 	fmt.Println("Init project")
 }
 
 func main() {
-	//
+	singleton = pkg.NewSingleton(singleton, "First")
+	singleton.Print()
+
+	singleton = pkg.NewSingleton(singleton, "Second")
+	singleton.Print()
 }
